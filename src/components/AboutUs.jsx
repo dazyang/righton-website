df26749515b5dda376bb843aaeebec97d9d4af14
dazyang <dazyang@protonmail.com>
2018-07-03 17:18:15 +1200

    adding AboutUs page

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -4,6 +4,33 @@ class AboutUs extends Component {
   render() {
     return (
       <div>
+        <div>Image</div>
+        <div>
+          <h3>Mission</h3>
+          <p>Our mission is to make math more fun for middle and high school students, increasing both motivation and self-confidence. Not a big fan of math? Great, RightOn! is the game for you. Already like math? That’s cool too, you’ll like it even more!</p>
+        </div>
+        <div>
+          <h3>Why RightOn!</h3>
+          <p>RightOn! is a free game show app that makes math more fun for middle and high school students.</p>
+          <p>
+            <h4>Increasing educational equity in STEM</h4>
+            The same high-quality, fun version of RightOn! is available for all students – no matter your school, zip code, or how much you even like math.
+
+            <h4>Fostering mathematical mindsets</h4>
+            Everyone can get better at math, especially when you try. We focus on wrong answers rather than right ones, so anyone who has ever come up with a wrong answer can play this game!
+
+            <h4>Having fun with math</h4>
+            RightOn! Is not just any game, it’s a game show where you can play in teams and win prizes – real ones!
+          </p>
+        </div>
+        <div>
+          <h3>Team</h3>
+
+        </div>
+        <div>
+          <h3>Advisors</h3>
+          
+        </div>
       </div>
     )
   }
