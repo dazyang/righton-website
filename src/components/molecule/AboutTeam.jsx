1c78ed1c412a5d918604b5d929fe603f45548a47
dazyang <dazyang@protonmail.com>
2018-07-10 17:49:33 +1200

    fixed flex-flow in single-profile

--- src/components/molecule/AboutTeam.jsx
+++ src/components/molecule/AboutTeam.jsx
@@ -14,27 +14,27 @@ function AboutTeam(){
         <div className='col-wrapper'>
           <center>
             <img src={callan} className='profile-photo' alt='Callan Stretton is a full-stack developer at RightOn! Education.' />
-            <h4 className='three-col-title'>Callan Stretton</h4>
+            <h4 >Callan Stretton</h4>
             <h6 className='job-title'>Full-Stack Developer</h6>
-            </center>
+          </center>
           <center>
             <img src={daz} width='80%' className='profile-photo' alt='Daz C. Yang is a UI Designer & Front-End Developer at RightOn! Education.' />
-            <h4 className='three-col-title'>Daz C. Yang</h4>
+            <h4>Daz C. Yang</h4>
             <h6 className='job-title'>UI Designer / Front-End Developer</h6>
           </center>
           <center>
             <img className='profile-photo' src={edward} alt='' />
-            <h4 className='three-col-title'>Edward Tan</h4>
+            <h4>Edward Tan</h4>
             <h6 className='job-title'>Tech Lead</h6>
           </center>
           <center>
             <img className='profile-photo' src={kai} alt='' />
-            <h4 className='three-col-title'>Kai King</h4>
+            <h4>Kai King</h4>
             <h6 className='job-title'>Math Educator</h6>
           </center>
           <center>
             <img src={sinclair} className='profile-photo' alt='Sinclair Wu is the product lead at RightOn! Education.' />
-            <h4 className='three-col-title'>Sinclair Wu</h4>
+            <h4>Sinclair Wu</h4>
             <h6 className='job-title'>Product Lead</h6>
           </center>
         </div>
