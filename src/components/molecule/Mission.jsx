c62388dc84e2558c702fddf1968f550e8c7f9740
dazyang <dazyang@protonmail.com>
2018-07-11 13:09:55 +1200

    added media queries for mobile

--- src/components/molecule/Mission.jsx
+++ src/components/molecule/Mission.jsx
@@ -6,11 +6,13 @@ import plus from '../../images/plus_icon.svg';
 function Mission () {
     return (
       <section>
-        <img src={diversity} alt="CHANGE IMAGE" width='70%' />
+        <div className='mission-image'>
+          <img src={diversity} alt="CHANGE IMAGE" width='100%' />
+          </div>
         <div className='card-wrapper mission-card'>
-          <h2><img src={plus} className='about-icons' alt='product feature' />Mission</h2>
+          <h2><img src={plus} className='about-icons' alt='mission' />Mission</h2>
           <h3>Our mission is to make math more fun for middle and high school students, increasing both motivation and self-confidence. Not a big fan of math? Great, <b><em>RightOn!</em></b> is the game for you. Already like math? That’s cool too, you’ll like it even more!</h3>
-        </div>
+          </div>
       </section>
     )
 }
