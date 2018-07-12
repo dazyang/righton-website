46534eb1fcdef965ee09c25e012a7b3322679fec
dazyang <dazyang@protonmail.com>
2018-07-12 16:22:06 +1200

    updated footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
-import x_icon from '../images/x_icon.svg';
+import righton from '../righton.svg'
 
 class Footer extends Component {
   render() {
     return (
       <footer>
         {/* <a href="mailto:sincwu@gmail.com"><button className='sign-up'>Subscribe to our newsletter</button></a> */}
-        <img src={x_icon} width='8%' alt='product feature' />
-        <h1>Stay tuned!</h1>
+          <p className='copyright-msg'>2018 RightOn Education &copy; All Rights Reserved</p>
+        <div className='footer-icon'>
+          <img src={righton} width='100%' alt='RightOn logo' />
+        </div>
+        <div className='footer-message'>
+          <h3><b>Interested in learning more?</b><br/> Email us at <a href="mailto:info@rightoneducation.com" className='email-us'>info@rightoneducation.com</a></h3>
+        </div>
       </footer>
     )
   }
