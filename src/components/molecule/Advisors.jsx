858709a99ce05a03af2f99306bdb6708cd0fb5cc
dazyang <dazyang@protonmail.com>
2018-07-24 15:33:24 +1200

    updated advisors

--- src/components/molecule/Advisors.jsx
+++ src/components/molecule/Advisors.jsx
@@ -11,21 +11,21 @@ function Advisors() {
       <h2><img src={xicon} className='about-icons' alt='product feature' />Advisors</h2>
       <div className='col-wrapper'>
         <div className='single-profile'>
-          <img src={AyNur} className='profile-photo-advisors' alt='Callan Stretton is a full-stack developer at RightOn! Education.' />
+          <img src={AyNur} className='profile-photo-advisors' alt='' />
           <center>
             <h2 className='h2-light'>Ay-Nur Najm</h2>
             <h5>Math Instructional Coach</h5>
           </center>
         </div>
         <div className='single-profile'>
-          <img src={Michael} className='profile-photo-advisors' alt='Callan Stretton is a full-stack developer at RightOn! Education.' />
+          <img src={Michael} className='profile-photo-advisors' alt='' />
           <center>
             <h2 className='h2-light'>Michael Hwa</h2>
-            <h5>Managing Director at Accenture</h5>
+            <h5>Managing Director, Accenture</h5>
           </center>
         </div>
         <div className='single-profile'>
-          <img src={Ben} className='profile-photo-advisors' alt='Callan Stretton is a full-stack developer at RightOn! Education.' />
+          <img src={Ben} className='profile-photo-advisors' alt='' />
           <center>
             <h2 className='h2-light'>Ben Woodford</h2>
             <h5>Math Education Researcher</h5>
