4bb7cc67afcf3a44907d17b75d7067eeb1d97d47
Callan Stretton <callanstretton@Callans-MacBook-Pro.local>
2018-08-21 18:07:48 +1200

    changed hashrouter to browserrouter

--- src/components/molecule/Header.jsx
+++ src/components/molecule/Header.jsx
@@ -2,25 +2,25 @@ import React, { Component } from 'react'
 import { HashLink as Link } from 'react-router-hash-link'
 
 class Header extends Component {
-  render() {
+  render () {
     return (
-        <header className='grid-12 grid-tl-12 grid-d-12 grid-m-12'>
-          <div className='nav-options'>
-            <Link to='/about'><h4>About</h4></Link>
-          </div>
-          <div className='nav-options'>
+      <header className='grid-12 grid-tl-12 grid-d-12 grid-m-12'>
+        <div className='nav-options'>
+          <Link to='/about'><h4>About</h4></Link>
+        </div>
+        <div className='nav-options'>
           <Link to='/#features'>
-              <h4>Product</h4>
-            </Link>
-          </div>
-          <div className='nav-options'>
-            <Link to='/#footer'>
-              <h4>Contact</h4>
-            </Link>
-          </div>
-        </header>
+            <h4>Product</h4>
+          </Link>
+        </div>
+        <div className='nav-options'>
+          <Link to='/#footer'>
+            <h4>Contact</h4>
+          </Link>
+        </div>
+      </header>
     )
   }
 }
 
-export default Header;
+export default Header
