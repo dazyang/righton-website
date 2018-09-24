7917dbef3272577f295ef6db8e27e685f85d5ae2
dazyang <dazyang@protonmail.com>
2018-09-24 13:36:59 +1200

    added link to logo at the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ class Footer extends Component {
           <p className='copyright-msg'>RightOn Education &copy; 2018 All Rights Reserved
           </p>
           <div className='footer-logo'>
-            <img src={righton} width='100%' alt='RightOn logo' />
+            <a href='/'><img src={righton} width='100%' alt='RightOn logo' /></a>
           </div>
           <div className='footer-msg'>
             <h3><b>Interested in learning more?</b><br/> Email us at <a href="mailto:info@rightoneducation.com" className='email-us'>info@rightoneducation.com</a></h3>
